7108ccfe9f0a33eac778ac18525b903e887f2592
ravi-kenko <ravi.goswami@kenko-health.in>
2022-07-24 14:20:53 +0530

    Initial commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Ravi Goswami on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
